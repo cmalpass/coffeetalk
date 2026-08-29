008b3a2dbe5fa5aced15f94249af202d21e69de7
cmalpass <cmalpass@users.noreply.github.com>
2026-08-29 16:20:09 -0400

    Add CLI stop support for interrupting conversations (#166)

--- CoffeeTalk.Core/Services/AgentConversationOrchestrator.cs
+++ CoffeeTalk.Core/Services/AgentConversationOrchestrator.cs
@@ -380,6 +380,7 @@ public class AgentConversationOrchestrator
             {
                 if (_ui.StopRequested)
                 {
+                    _ui.TerminationReason = ConversationTerminationReason.UserStopped;
                     return;
                 }
 
